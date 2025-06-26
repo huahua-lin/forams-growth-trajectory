2f9bf38d1689eaddc538dd1e3d80eb7bff91dabd
huahua-lin <huahualin0905@gmail.com>
2025-06-26 15:11:08 +0100

    reduce duplication to the utils

--- instance_seg/unet_sw/seeded_watershed.py
+++ instance_seg/unet_sw/seeded_watershed.py
@@ -1,14 +1,11 @@
 import argparse
 import os
-from typing import Dict, List
 
 import numpy as np
-from PIL import Image
 from skimage.morphology import isotropic_erosion
 import cc3d
-import csv
 from skimage.segmentation import watershed
-from utils import stack_imgs
+from utils import stack_imgs, save_chamber_info, save_slice_by_slice
 
 
 def seeded_watershed(mask: np.array, erosion=True, erosion_radius=None, ccl_conn=6, seed_size=10) -> [np.ndarray,
@@ -51,45 +48,6 @@ def seeded_watershed(mask: np.array, erosion=True, erosion_radius=None, ccl_conn
     return watershed_result, seeds
 
 
-def save_csv(file_name: str, num_centroids_dict: Dict[str, int], volumes_dict: Dict[str, List[int]],
-             centroids_dict: Dict[str, List[List[float]]]):
-    """
-    Save chamber information, including the number of chambers, volumes and centroids.
-
-    Args:
-        file_name: Name of the output CSV file.
-        num_centroids_dict: Dictionary mapping chamber names to the number of centroids.
-        volumes_dict: Dictionary mapping chamber names to their volume.
-        centroids_dict: Dictionary mapping chamber names to a list of centroid coordinates.
-
-    """
-    header = ["name", "num_chambers", "volumes", "centroids"]
-    with open(file_name, mode="w", newline="") as csvfile:
-        writer = csv.writer(csvfile)
-        writer.writerow(header)
-        for key, value in centroids_dict.items():
-            row = [key] + [str(num_centroids_dict[key])] + [str(volumes_dict[key])] + [str(centroids_dict[key])]
-            writer.writerow(row)
-
-
-def save_sw(sample_name: str, watershed_result: np.ndarray):
-    """
-    Save instance segmentation results.
-
-    Args:
-        sample_name: Name of the sample.
-        watershed_result: Instance segmentation result.
-
-    """
-    if not os.path.exists(os.path.join(args.save_img_pth, sample_name)):
-        os.makedirs(os.path.join(args.save_img_pth, sample_name))
-
-    # Save slice by slice (0.png, 1.png, ...)
-    for i in range(watershed_result.shape[2]):
-        mask_img = Image.fromarray(watershed_result[:, :, i].astype(np.uint8))
-        mask_img.save(os.path.join(args.save_img_pth, os.path.join(sample_name, str(i) + ".png")))
-
-
 def run():
     sample_names = os.listdir(args.root)
 
@@ -102,7 +60,7 @@ def run():
         mask = stack_imgs(os.path.join(args.root, sample_name))
 
         # perform seeded watershed on the 3d data (radius 3 for 2D; radius 2 for 3D)
-        watershed_result, seeds = seeded_watershed(mask, erosion=False, erosion_radius=2, seed_size=10)
+        watershed_result, seeds = seeded_watershed(mask, erosion=True, erosion_radius=2, seed_size=10)
 
         # calculate chamber geo info: num. of chambers, volumes per chamber, centroids per chamber
         unique_labels = np.unique(seeds)  # seeds or watershed
@@ -123,7 +81,7 @@ def run():
         # save instance segmentation results
         if args.save_img:
             print("Saving images...")
-            save_sw(sample_name, watershed_result)
+            save_slice_by_slice(os.path.join(args.save_img_pth, sample_name), watershed_result, dim=2, format=".png")
             print("Images saved.")
 
         print(sample_name)
@@ -135,19 +93,19 @@ def run():
     # save chamber information to the CSV file
     if args.save_csv:
         print("Saving CSV file...")
-        save_csv(args.save_csv_pth, num_centroids_dict, volumes_dict, centroids_dict)
+        save_chamber_info(args.save_csv_pth, num_centroids_dict, volumes_dict, centroids_dict)
         print("CSV file saved.")
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description="Seeded watershed on binary masks")
-    parser.add_argument("--root", type=str, default=".",
+    parser.add_argument("--root", type=str, default="D:/PISTON/pred/biseg_unet3d/test",
                         help="Path of all semantic segmentation results divided by samples")
     parser.add_argument("--save-csv", type=bool, default=True,
                         help="Save chamber information to the csv file")
     parser.add_argument("--save-csv-pth", type=str, default="./chambers_geo.csv",
-                        help="File name of csv")
-    parser.add_argument("--save-img", type=bool, default=False,
+                        help="Path to save csv file")
+    parser.add_argument("--save-img", type=bool, default=True,
                         help="Save instance segmentation results")
     parser.add_argument("--save-img-pth", type=str, default="./",
                         help="Path to save the instance segmentation results")
